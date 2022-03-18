4a61cdb57ac7685f33343ea8614958f390804076
“BishrJones” <bishr6jones@gmail.com>
2022-03-18 08:44:44 -0700

    Show page render

---
+++ README.md
@@ -0,0 +1,33 @@
+# Project-2 (jabberstock)
+
+## Description:
+
+A stock application that allows you to research investments as well as have discourse with the community on said investments.
+
+---
+
+## technology Used:
+
+- bcryptjs
+- express
+- dotenv
+- method-override
+- morgan
+- mongoose
+- liquid-express-views
+- css
+- javascript
+- api: https://www.stockdata.org/
+
+---
+
+## User story:
+
+- Signup and login to enter the application and participate in the features.
+- User is able to search up a stock symbol to complete research about the potential investment
+- When the stock is searched the company, price, and more relavent info will appear
+- Once the stock is searched user is able to right comments about the stock.
+- User is able to save the stock in their own account
+
+![alt](README/erd.png)
+![alt](README/wireframe.png)
--- controllers/stock.js
+++ controllers/stock.js
@@ -3,6 +3,7 @@ const express = require('express')
 const Stock = require('../models/stock')
 const axios = require('axios')
 const { Query } = require('../models/connection')
+const { response } = require('express')
 require("dotenv").config()
 // Create router
 const router = express.Router()
@@ -30,131 +31,50 @@ const reqUrlBack = `&api_token=${apiKey}`
 // Routes
 
 // index ALL
-// const requestUrl = 'https://api.stockdata.org/v1/data/quote?symbols=NKE&api_token=vxxRgceTwP37L5OQ4gedlvfB5oNicVcfV3LiXgOZ'
 
 const requestUrl =`${reqUrlFront}${stockSymbol}${reqUrlBack}`
-// const populateData = (data) => {resposeData}
 
 router.get('/', (req,res)=>{
 	Stock.find({})
 	axios(requestUrl)
 		.then((responseData)=>{
-			const stockData = responseData.data.data
+			const stock = responseData.data.data
 			console.log('this is the response date: \n', responseData)
 			// return responseData.json()
-			res.render('stock/index', {stocks: stockData}) 
+			res.render('stock/index', {stocks: stock}) 
 			// res.send(stockData)
 			
 		})
-		// .then((jsonData)=>{
-		// 	console.log('here is the data:', jsonData)
-		// 	console.log(jsonData.data)
-		// })
+		
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
 		})
 	
 })
 
+router.post('/stock', (req,res)=>{
+	
+	res.redirect()
+})
 
-// router.get('/', (req, res) => { 
-// 	Example.find({})
-// 		.then(examples => {
-// 			const username = req.session.username
-// 			const loggedIn = req.session.loggedIn
+// show route
+router.get('/:stocktick', (req,res) => {
+	const stockId = req.params.ticker
+	console.log(stockId)
+	Stock.find(stockId)
+
+		.then((stock)=>{
 			
-// 			res.render('examples/index', { examples, username, loggedIn })
-// 		})
-// 		.catch(error => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
+			
+			res.render('stock/show', {stocks: stock})
+			
+		})
+		.catch(error =>{
+			res.redirect(`/error?error=${error}`)
+		})
 
-// // index that shows only the user's examples
-// router.get('/mine', (req, res) => {
-//     // destructure user info from req.session
-//     const { username, userId, loggedIn } = req.session
-// 	Example.find({ owner: userId })
-// 		.then(examples => {
-// 			res.render('examples/index', { examples, username, loggedIn })
-// 		})
-// 		.catch(error => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
 
-// // new route -> GET route that renders our page with the form
-// router.get('/new', (req, res) => {
-// 	const { username, userId, loggedIn } = req.session
-// 	res.render('examples/new', { username, loggedIn })
-// })
+})
 
-// // create -> POST route that actually calls the db and makes a new document
-// router.post('/', (req, res) => {
-// 	req.body.ready = req.body.ready === 'on' ? true : false
 
-// 	req.body.owner = req.session.userId
-// 	Example.create(req.body)
-// 		.then(example => {
-// 			console.log('this was returned from create', example)
-// 			res.redirect('/examples')
-// 		})
-// 		.catch(error => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
-// // edit route -> GET that takes us to the edit form view
-// router.get('/:id/edit', (req, res) => {
-// 	// we need to get the id
-// 	const exampleId = req.params.id
-// 	Example.findById(exampleId)
-// 		.then(example => {
-// 			res.render('examples/edit', { example })
-// 		})
-// 		.catch((error) => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
-// // update route
-// router.put('/:id', (req, res) => {
-// 	const exampleId = req.params.id
-// 	req.body.ready = req.body.ready === 'on' ? true : false
-
-// 	Example.findByIdAndUpdate(exampleId, req.body, { new: true })
-// 		.then(example => {
-// 			res.redirect(`/examples/${example.id}`)
-// 		})
-// 		.catch((error) => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
-// // show route
-// router.get('/:id', (req, res) => {
-// 	const exampleId = req.params.id
-// 	Example.findById(exampleId)
-// 		.then(example => {
-//             const {username, loggedIn, userId} = req.session
-// 			res.render('examples/show', { example, username, loggedIn, userId })
-// 		})
-// 		.catch((error) => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
-// // delete route
-// router.delete('/:id', (req, res) => {
-// 	const exampleId = req.params.id
-// 	Example.findByIdAndRemove(exampleId)
-// 		.then(example => {
-// 			res.redirect('/examples')
-// 		})
-// 		.catch(error => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
-
-// Export the Router
 module.exports = router
